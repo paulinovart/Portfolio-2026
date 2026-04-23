d05968b8168ba6e12ae089c2120a6326cd839203
paulinovart <pauline.dierick@gmail.com>
2026-04-23 20:47:53 +0200

    animations

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const projects = document.querySelectorAll(".home-page-project");
+const workExperienceElementTitles = document.querySelectorAll(".work-experience-element-title");
+const formationElementTitles = document.querySelectorAll(".formation-element-title");
+const seeElementArrows = document.querySelectorAll(".see-element-arrow");
+const workExperienceElements = document.querySelectorAll(".work-experience-element");
+const formationElements= document.querySelectorAll(".formation-element");
+
+//redirection vers les pages des projets spécifiques quand on clique dessus
 
 projects.forEach(project => {
   project.addEventListener("click", () => {
@@ -8,3 +15,46 @@ projects.forEach(project => {
     window.location.href = `project.html?id=${id}`;
   });
 });
+
+
+//menu déroulant des experiences professionnelles
+
+workExperienceElementTitles.forEach (element => {
+    
+    element.addEventListener("click", () =>{
+
+       
+    const parent = element.closest(".work-experience-element");
+    const p = parent.querySelector(".work-experience-description");
+
+    p.classList.toggle("invisible");
+    } )
+    
+});
+
+//menu déroulant formations
+
+formationElementTitles.forEach (element => {
+    
+    element.addEventListener("click", () =>{
+
+       
+    const parent = element.closest(".formation-element");
+    const p = parent.querySelector(".formation-description");
+
+    p.classList.toggle("invisible");
+
+
+    } );
+
+/*element.addEventListener("mouseover", ()=>{
+
+    const seeElementArrow = document.querySelector(".see-element-arrow");
+    seeElementArrow
+
+})*/
+
+
+    
+});
+
